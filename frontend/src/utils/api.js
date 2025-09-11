ff2b5bd88c51d0bd9ea25344841bbff60a6f91a2
HafsaSaleem069 <hafsasaleem152ics@gmail.com>
2025-09-11 12:36:38 +0500

    fix: api util

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,19 +1,18 @@
-
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
-  withCredentials: true // Important for sending cookies
+  baseURL: process.env.NEXT_PUBLIC_API_URL + "/api", // ✅ use env variable
+  withCredentials: true, // important for cookies
 });
 
-/*api.interceptors.request.use((config) => {
+/*
+api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
-});*/
-
+});
+*/
 
 export default api;
-

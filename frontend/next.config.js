41094053aa02c54fdb346125334e0e2b21f7ee61
HafsaSaleem069 <hafsasaleem152ics@gmail.com>
2025-09-11 12:24:27 +0500

    fix:railway backend deployed

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/register/:path*",
-        destination:
-          "https://globuy-ecommerce-admin-customer-production.up.railway.app/register/:path*", // <-- Replace with Railway/Render/Backend URL
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/register/:path*",
+  //       destination:
+  //         "https://globuy-ecommerce-admin-customer-production.up.railway.app/register/:path*", // <-- Replace with Railway/Render/Backend URL
+  //     },
+  //   ];
+  // },
   eslint: {
     ignoreDuringBuilds: true,
   },
